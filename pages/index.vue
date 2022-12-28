fcc87e52f6057625c8c83d4f915d94b099d6dcb9
unknown <asunov.artur.2007@gmail.com>
2022-12-28 23:45:08 +0300

    dff

--- pages/index.vue
+++ pages/index.vue
@@ -311,7 +311,7 @@ export default {
             data: [],
             name: 'Продажи'
           },]}
-      let
+      let task1 = +window.localStorage.getItem('task1'),
           token = window.localStorage.getItem('access');
         console.log(token);
       this.$store.dispatch('request/get_all_retail', {userId: this.userId}).then((x) => {
@@ -329,15 +329,14 @@ export default {
                 x.data['product']['products'].map(i => {
                   console.log(i['date_seller']);
                   newChart.categories.push(i['date_seller']);
-                  newChart.series[0].data.push(i['cnt']);
+                  newChart.series[1].data.push(i['cnt']);
                 })
               }
-
               this.$store.dispatch('request/get_order_data', {userId: this.userId, graph: true, config: this.config, dateFrom: "2022-11-01", flag: '0', type: 3,}).then((x) => {
                 if(x.data.success){
                   console.log()
                   x.data['product']['products'].map(i => {
-                    newChart.series[1].data.push(i['cnt']);
+                    newChart.series[0].data.push(i['cnt']);
                   })
                 }
                 this.chart = newChart;
@@ -357,6 +356,7 @@ export default {
             })
             this.order.count = x.data['product']['count'][0]['cnt'];
             this.order.total = x.data['product']['total'][0]['cnt'];
+
           }
           console.log(this.order);
 
